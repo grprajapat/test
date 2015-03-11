863f4476e3ba4a86f382e062c35cc3548698dc92
grprajapat <grprajapat@ymail.com>
2015-03-11 11:46:54 +0530

    Create something.php

---
+++ something.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Global College of Technology Jaipur</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="logincss/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Custom Fonts -->
+   
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style type="text/css">
+    .navbar-bottom {
+    min-height: 50px;
+    margin-top: 10px;
+    background-color: #28364f;
+    padding-top: 5px;
+    color:#FFFFFF;
+    }
+    </style>
+	
+	
+	
+</head>
+
+
+<body background="paperless/true.jpg">
+
+
+
+    <div align="center" style="height:100px; padding-top:50px; margin-bottom:53px;">
+        <img src="paperless/3106896.png">
+    </div>
+	
+    
+    <div align="center" style="height:40px; width:100%; font-size:20px; color:#428BCA; padding-bottom:15px;">
+        <strong><font color="#FFFFFF">Global College of Technology Jaipur</font></strong>
+    </div>
+
+   <div class="container">
+    <div class="row">
+         <div class="col-sm-6 col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading"> <strong class="">Login</strong>
+
+                </div>
+                <div class="panel-body">
+                    
+					<form action="loginproc.php" method="post"  role="form" class="form-horizontal">
+					    <div align="center" style="padding-bottom:10px;"> </div>
+                        <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-3 control-label">Username</label>
+                            <div class="col-sm-9">
+                                <input type="username" name="username" class="form-control" id="username" placeholder="username" required="">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputPassword3" class="col-sm-3 control-label">Password</label>
+                            <div class="col-sm-9">
+                                <input type="password" name="password" class="form-control" id="password" placeholder="Password" required="">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-9">
+                                <div class="checkbox">
+                                    <label class="">
+                                        <input type="checkbox" class="">Remember me</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group last">
+                            <div class="col-sm-offset-3 col-sm-9">
+                             
+								<input  type="submit"  name="submit" value='Login' class="btn btn-success btn-sm">
+                               
+								<button type="reset" class="btn btn-default btn-sm">Reset</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div align="center" class="panel-footer"><a href="" title="GCT IT Team">Developed by-<strong> GCT IT Team</strong></a>
+                </div>
+				 
+            </div>
+        </div><img align="left" src="paperless/go-green-go-paperless.png" height="11%" width="11%">
+
+    </div>
+</div>
+
+   
+</body>
+
+</html>
